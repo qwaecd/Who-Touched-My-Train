9bcf9c42d419ab9e372b22f4af44a981247758e1
qwaecd <200468713+qwaecd@users.noreply.github.com>
2026-01-16 14:30:16 +0800

    refactor(items): 重构物品注册系统并添加工具提示
    - 将物品注册从RegistryObject改为ItemEntry，使用CreateRegistrate进行管理
    - 移除独立的创造模式标签页类，集成到主类中统一管理
    - 为所有物品添加详细的工具提示信息，包括功能描述和使用方法
    - 更新依赖版本至Create 6.0.8和其他相关库版本
    - 修改模块版本号从1.2.1-beta-6.0到1.3.0-Create6.0
    - 优化代码结构，移除冗余的导入和注册逻辑

--- src/main/java/com/qwaecd/wtmt/WhoTouchedMyTrain.java
+++ src/main/java/com/qwaecd/wtmt/WhoTouchedMyTrain.java
@@ -1,26 +1,37 @@
 package com.qwaecd.wtmt;
 
-import com.mojang.logging.LogUtils;
 import com.qwaecd.wtmt.init.AllModItems;
-import com.qwaecd.wtmt.init.ModCreativeModeTab;
 import com.qwaecd.wtmt.network.AllSerializers;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.simibubi.create.foundation.item.ItemDescription;
+import com.simibubi.create.foundation.item.KineticStats;
+import com.simibubi.create.foundation.item.TooltipModifier;
+import net.createmod.catnip.lang.FontHelper;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.slf4j.Logger;
 
 @Mod(WhoTouchedMyTrain.MOD_ID)
 public class WhoTouchedMyTrain
 {
     public static final String MOD_ID = "who_touched_my_train";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
+    public static final CreateRegistrate REGISTRATE = CreateRegistrate.create(MOD_ID)
+            .defaultCreativeTab("wtmt_tab",
+                    t -> {
+                        t.icon(() -> AllModItems.GOLD_KEY.get().getDefaultInstance());
+                        t.title(Component.translatable("who_touched_my_train.wtmt_tab"));
+                    }).build()
+            .setTooltipModifierFactory(item ->
+                    new ItemDescription.Modifier(item, FontHelper.Palette.STANDARD_CREATE)
+                            .andThen(TooltipModifier.mapNull(KineticStats.create(item)))
+            );
     public WhoTouchedMyTrain(FMLJavaModLoadingContext context)
     {
         IEventBus modEventBus = context.getModEventBus();
-        AllModItems.registerAllItems(modEventBus);
+        AllModItems.registerAllItems();
         AllSerializers.register(modEventBus);
-        ModCreativeModeTab.registerAll(modEventBus);
+        REGISTRATE.registerEventListeners(modEventBus);
 //        Channel.register();
     }
 }
--- src/main/java/com/qwaecd/wtmt/init/AllModItems.java
+++ src/main/java/com/qwaecd/wtmt/init/AllModItems.java
@@ -1,38 +1,40 @@
 package com.qwaecd.wtmt.init;
 
+import com.qwaecd.wtmt.WhoTouchedMyTrain;
 import com.qwaecd.wtmt.item.TrainLock;
 import com.qwaecd.wtmt.item.key.AdminTrainKey;
 import com.qwaecd.wtmt.item.key.GoldTrainKey;
 import com.qwaecd.wtmt.item.key.IronTrainKey;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.network.chat.Component;
-import net.minecraft.world.item.CreativeModeTab;
-import net.minecraft.world.item.CreativeModeTabs;
+import com.simibubi.create.foundation.data.CreateRegistrate;
+import com.tterrag.registrate.builders.ItemBuilder;
+import com.tterrag.registrate.util.entry.ItemEntry;
+import com.tterrag.registrate.util.nullness.NonNullFunction;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.function.Supplier;
+import java.util.function.Consumer;
 
 import static com.qwaecd.wtmt.WhoTouchedMyTrain.MOD_ID;
 
 public final class AllModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<Item> TRAIN_LOCK = register("train_lock", () -> new TrainLock(new Item.Properties()));
-    public static final RegistryObject<Item> IRON_KEY = register("iron_key", () -> new IronTrainKey(new Item.Properties()));
-    public static final RegistryObject<Item> GOLD_KEY = register("gold_key", () -> new GoldTrainKey(new Item.Properties()));
-    public static final RegistryObject<Item> ADMIN_KEY = register("admin_key", () -> new AdminTrainKey(new Item.Properties()));
+    public static final ItemEntry<TrainLock> TRAIN_LOCK = register("train_lock", TrainLock::new);
+    public static final ItemEntry<IronTrainKey> IRON_KEY = register("iron_key", IronTrainKey::new);
+    public static final ItemEntry<GoldTrainKey> GOLD_KEY = register("gold_key", GoldTrainKey::new);
+    public static final ItemEntry<AdminTrainKey> ADMIN_KEY = register("admin_key", AdminTrainKey::new);
 
-    public static void registerAllItems(IEventBus modEventBus) {
-        ITEMS.register(modEventBus);
+    public static void registerAllItems() {
     }
 
-    private static <T extends Item> RegistryObject<Item> register(String itemName, Supplier<T> itemSupplier) {
-        return ITEMS.register(itemName, itemSupplier);
+    private static <T extends Item> ItemEntry<T> register(String itemName,  NonNullFunction<Item.Properties, T> factory) {
+        return WhoTouchedMyTrain.REGISTRATE.item(itemName, factory).register();
+    }
+
+    private static <T extends Item> ItemEntry<T> register(String itemName, NonNullFunction<Item.Properties, T> factory, Consumer<ItemBuilder<T, CreateRegistrate>> modifier) {
+        ItemBuilder<T, CreateRegistrate> itemBuilder = WhoTouchedMyTrain.REGISTRATE.item(itemName, factory);
+        modifier.accept(itemBuilder);
+        return itemBuilder.register();
     }
 }
